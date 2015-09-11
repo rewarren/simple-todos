a6f5ce448f8c7b1a1098c7114628ddb4d9e2eb2d
Rachel Warren <raelwa@gmail.com>
2015-09-11 14:53:31 -0400

    sort tasks by newest first

--- simple-todos.js
+++ simple-todos.js
@@ -3,11 +3,12 @@ Tasks = new Mongo.Collection("tasks");
 if (Meteor.isClient) {
   Template.body.helpers({
     tasks: function(){
-      return Tasks.find({});
+      return Tasks.find({}, {sort: {createdAt: -1}});
     }
   });
 
   Template.body.events({
+    //listening to the submit event on any element that matches the css selector .new-task
     "submit .new-task": function (event){
       event.preventDefault();
       var text = event.target.text.value; //get value from form
