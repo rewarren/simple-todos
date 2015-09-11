80d8c9de97934298665ca746037eb90101325335
Rachel Warren <raelwa@gmail.com>
2015-09-11 14:35:43 -0400

    add js functionality for form input and value retrieval

--- simple-todos.js
+++ simple-todos.js
@@ -6,4 +6,16 @@ if (Meteor.isClient) {
       return Tasks.find({});
     }
   });
+
+  Template.body.events({
+    "submit .new-task": function (event){
+      event.preventDefault();
+      var text = event.target.text.value; //get value from form
+      Tasks.insert({
+        text: text,
+        createdAt: new Date() //current time
+      });
+      event.target.text.value = ""; //clear form
+    }
+  });
 }
